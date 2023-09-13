24081321ba7098959b0aea985b42fe5115404e27
Luca Schulz <luca.schulz@tecalliance.net>
2023-09-13 09:52:23 +0200

    added afe uestion to program

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿namespace HelloWorld
+﻿using System.Numerics;
+using System.Security.Cryptography;
+
+namespace HelloWorld
 {
     class Program
     {
@@ -6,8 +9,11 @@
         {
             Console.WriteLine("Gebe mir deinen Namen!");
             string name = Console.ReadLine();
-            
-            Console.WriteLine("Hallo " + name + "!, Was geht du Opfer?");
+            Console.WriteLine("Wie alt bist du?");
+            string age = Console.ReadLine();
+            int  alter = Convert.ToInt16(age);
+            int  alterinzehn = alter + 10;
+            Console.WriteLine("Hallo " + name + "!, Was geht?" + " Du bist schon " + age + "?" + " Das heisst ja, dass du in 10 Jahren " + alterinzehn + " bist");
         }
     }
 }
