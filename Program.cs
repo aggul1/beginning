90d1da6d057c3ef0e94fde8972e461308b9cd974
Luca Schulz <luca.schulz@tecalliance.net>
2023-09-13 15:13:42 +0200

    no message

--- Program.cs
+++ Program.cs
@@ -8,14 +8,15 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             List<int> myNumberList = new List<int>();
-            Console.WriteLine("Sage mir wieviele Zahln du in deine Liste hinzufügen möchtest");
-            int amountNumbers = Convert.ToInt32(Console.ReadLine());
-
-            while(myNumberList.Count < amountNumbers)
+            int user_zahl = 0;
+            do
             {
+                //codeblock
                 Console.WriteLine("Gebe mir eine Zahl");
-                myNumberList.Add(Convert.ToInt32(Console.ReadLine()));
-            }
+                user_zahl = Convert.ToInt32(Console.ReadLine());
+                myNumberList.Add(user_zahl);
+            } while(user_zahl > 0);
+
             //Summiere alle Zahlen auf
             int ergebnis = 0;
             foreach(int zahl in myNumberList)
