ad30c36edc368e894f819dcec5273de69f97a9cd
Luca Schulz <luca.schulz@tecalliance.net>
2023-09-13 10:45:14 +0200

    make subtraction

--- Program.cs
+++ Program.cs
@@ -7,13 +7,15 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-        // Frage den Nutzer nach 2 Zahlen und gebe das Ergebnis auf der Console aus
-        Console.WriteLine("Welche beiden Zahlen möchtest du addieren?");
+        // Frage den Nutzer nach 2 Zahlen und gebe das Ergebnis auf der Console aus /Subtraktion)
+        Console.WriteLine("Welche 2 Zahlen möchtest du voneinander subtrahieren?");
         int zahl1 = Convert.ToInt16(Console.ReadLine());
         Console.WriteLine("Und?");
         int zahl2 = Convert.ToInt16(Console.ReadLine());
-        int ergebnis = zahl1 + zahl2;
-        Console.WriteLine($"Das Ergebnis aus {zahl1} und {zahl2} lautet {ergebnis}");
+        int ergebnis = zahl1 - zahl2;
+        Console.WriteLine($"{zahl1} - {zahl2} = {ergebnis}");
+
+
     }   
 }
 }
