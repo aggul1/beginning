4892ba53d527b2a2306dcef65e01a7216221b4df
Luca Schulz <luca.schulz@tecalliance.net>
2023-09-14 15:56:40 +0200

    fizzbuzz

--- Program.cs
+++ Program.cs
@@ -10,53 +10,30 @@ namespace HelloWorld
     {
         static void Main(string[] args){
         
-            //Aufgabe 11: Frage den Nutzer nach einer Zahl und zähle von dieser Rückwärts bis 0
 
-                // Console.WriteLine("Gib mir eine Zahl!");
-                // int counter = Convert.ToInt16(Console.ReadLine());
-                // while(counter>=0)
-                // {
-                //     Console.WriteLine(counter);
-                //     counter--;                 
-                // }
 
-            //Aufgabe 12: Laufe mit einer For-Schleife über die Namen der Azubis & Studenten. Wenn der Name des Azubis ein „e“ enthält, gebe den Namen aus, ansonsten nicht.
-
-    //       List<string> names = new List<string> {"Luca", "Katharina", "Hans", "Linh", "Marcia", "Benny", "Niklas", "Lea"};
-
-    //         foreach(string name in names)
-    //         {
-    //             if(name.Contains('e'))
-    //             {
-    //                 Console.WriteLine(name);
-    //             }
-    //         }
-    //     }
-    // }
-
-            //Aufgabe 12b: Wiederhole das Ganze und lass den Nutzer einen Buchstaben bestimmen, nach dem gesucht wird.
-
-            List<string> names = new List<string> {"Luca", "Katharina", "Hans", "Linh", "Marcia", "Benny", "Niklas", "Lea"};
-            Console.WriteLine("Welchen Buchstaben möchtest du assortieren?");
-            string letter = Console.ReadLine();
-            foreach(string name in names)
-            {
-                if(name.Contains(letter))
+                Console.WriteLine("Gib mir eine Zahl!");
+                int counter = Convert.ToInt16(Console.ReadLine());
+                int start = 0;
+                while(start<=counter)
                 {
-                    Console.WriteLine(name);
+                    Console.WriteLine(start);
+                    start++;  
+                    if(start%3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }      
+                    if(start%5 == 0)
+                {
+                    Console.WriteLine("Buzz");
                 }
-            }
+                    if(start%3 == 0 && start%5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                
+                }
+                }
         }
-    }
-}
-
-            //Aufgabe 13: Frage den Nutzer  wie viele Einträge er in einem int-Array haben möchte. Initialisiere ein int-Array von dieser Größe und befülle dieses Int Array in einer Schleife, indem du den Nutzer jedes Mal frägst, welche Zahl eingetragen werden soll.  
-
-            //           => Gebe anschließend alle Zahlen nacheinander nochmal aus
-
-            //Aufgabe 13b: Gehe das Int-array durch und gebe den Maximal-Wert aus, Minimal-Wert und den Durchschnitt.
- 
-
-
-
+        }
+        }
 
