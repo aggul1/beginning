1fe30beee1df7299f54cdd0d87cf556d0fc3fcdc
Luca Schulz <luca.schulz@tecalliance.net>
2023-09-14 15:01:59 +0200

    aufgabe 12

--- Program.cs
+++ Program.cs
@@ -12,17 +12,27 @@ namespace HelloWorld
         
             //Aufgabe 11: Frage den Nutzer nach einer Zahl und zähle von dieser Rückwärts bis 0
 
-                Console.WriteLine("Gib mir eine Zahl!");
-                int counter = Convert.ToInt16(Console.ReadLine());
-                while(counter>=0)
-                {
-                    Console.WriteLine(counter);
-                    counter--;                 
-                }
+                // Console.WriteLine("Gib mir eine Zahl!");
+                // int counter = Convert.ToInt16(Console.ReadLine());
+                // while(counter>=0)
+                // {
+                //     Console.WriteLine(counter);
+                //     counter--;                 
+                // }
 
             //Aufgabe 12: Laufe mit einer For-Schleife über die Namen der Azubis & Studenten. Wenn der Name des Azubis ein „e“ enthält, gebe den Namen aus, ansonsten nicht.
 
+          List<string> names = new List<string> {"Luca", "Katharina", "Hans", "Linh", "Marcia", "Benny", "Niklas", "Lea"};
 
+            foreach(string name in names)
+            {
+                if(name.Contains('e'))
+                {
+                    Console.WriteLine(name);
+                }
+            }
+        }
+    }
 
             //Aufgabe 12b: Wiederhole das Ganze und lass den Nutzer einen Buchstaben bestimmen, nach dem gesucht wird.
 
@@ -35,5 +45,4 @@ namespace HelloWorld
 
 
 }
-}
-}
+
