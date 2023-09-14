28a716ac3fd6ed8ce0c88471e033c8959ffb1fc9
Luca Schulz <luca.schulz@tecalliance.net>
2023-09-14 11:11:56 +0200

    Aufgabe 8

--- Program.cs
+++ Program.cs
@@ -12,14 +12,21 @@ namespace HelloWorld
         
 
             //Aufgabe 7: Frage den Nutzer nach einem Text und ersetze im Text alle vorkommenden „U“ und „u“ durch „G“ und „g“
-            Console.WriteLine("Schreibe etwas!");
-            string mytext = Console.ReadLine();
-            string mytext2 = mytext.Replace("g","u").Replace("G","U");
 
-            Console.WriteLine(mytext2);
+            // Console.WriteLine("Schreibe etwas!");
+            // string mytext = Console.ReadLine();
+            // string mytext2 = mytext.Replace("g","u").Replace("G","U");
+            // Console.WriteLine(mytext2);
 
             //Aufgabe 8: Frage den Nutzer nach einer Texteingabe und fülle diese mit bis zu 20 Stellen mit einem y von links auf
+
+            string mytext = Console.ReadLine();
+            int anzahlb = mytext.Length;
+            string mytext2 = mytext.PadLeft(20,'y');
+            Console.WriteLine(mytext2);
+
             //Aufgabe 9: Frage den Nutzer nach einer Eingabe mit Semikolons ( ; ) und Splitte die Eingabe an diesem Zeichen. Gebe die einzelnen Werte auf der Console aus.
+            
             //Aufgabe 10: Frage den Nutzer nach einer Eingabe und überprüfe die Texteingabe des Nutzers, ob die Zeichen „abc“ in der Eingabe enthalten sind. Gebe true oder false auf der Konsole aus.
 
  
