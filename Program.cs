e3dad9ea8338c980ad8707b427448ed666d35231
Luca Schulz <luca.schulz@tecalliance.net>
2023-09-14 10:41:09 +0200

    Aufgabe 7

--- Program.cs
+++ Program.cs
@@ -10,59 +10,19 @@ namespace HelloWorld
     {
         static void Main(string[] args){
         
-        //Aufgabe 1: Initialisiere 2 Zahlen und gebe die Summe auf der Console aus
 
-        // int ergebnis = 1+1;
-        // Console.WriteLine(ergebnis);
+            //Aufgabe 7: Frage den Nutzer nach einem Text und ersetze im Text alle vorkommenden „U“ und „u“ durch „G“ und „g“
+            Console.WriteLine("Schreibe etwas!");
+            string mytext = Console.ReadLine();
+            string mytext2 = mytext.Replace("g","u").Replace("G","U");
 
+            Console.WriteLine(mytext2);
 
-            //Aufgabe 2: Lasse den Nutzer zwei Zahlen eingeben und gebe die Summe auf der Console aus
+            //Aufgabe 8: Frage den Nutzer nach einer Texteingabe und fülle diese mit bis zu 20 Stellen mit einem y von links auf
+            //Aufgabe 9: Frage den Nutzer nach einer Eingabe mit Semikolons ( ; ) und Splitte die Eingabe an diesem Zeichen. Gebe die einzelnen Werte auf der Console aus.
+            //Aufgabe 10: Frage den Nutzer nach einer Eingabe und überprüfe die Texteingabe des Nutzers, ob die Zeichen „abc“ in der Eingabe enthalten sind. Gebe true oder false auf der Konsole aus.
 
-            // Console.WriteLine("Gebe 2 Zahlen an");
-            // int zahl1 = Convert.ToInt16(Console.ReadLine());
-            // int zahl2 = Convert.ToInt16(Console.ReadLine());
-            // int ergebnis = zahl1 + zahl2;
-            // Console.WriteLine($"Das Ergebnis lautet: {ergebnis}");
-            
-
-            //Aufgabe 3: Lasse den Nutzer eine Zahl eingeben und gebe alle Zahlen
-            //           bis einschließlich der eingegeben Zahl aus
-
-            // Console.WriteLine("Gib eine Zahl an");
-            // int startwert = 0;
-            // int userzahl = Convert.ToInt16(Console.ReadLine());
-            // while(startwert<=userzahl)
-            // {
-                
-            //     Console.WriteLine(startwert);
-            //     startwert++;
-
-            // }
-
-            //Aufgabe 4: Lasse den Nutzer eine Zahl eingeben und gebe nur die geraden Zahlen aus.
-            //           (Hinweis: Modulo operator. => Geteilt durch 2,
-            //                  wenn der Rest 0 ist, ist die Zahl gerade)
-
-            Console.WriteLine("Gib eine Zahl an");
-            int startwert = 0;
-            int userzahl = Convert.ToInt16(Console.ReadLine());
-
-            for (startwert = 0; startwert <= userzahl; startwert++)
-            {
-                if (startwert % 2 ==0)
-                {
-                    Console.WriteLine(startwert);
-                }
-
-            }
-
-            //Aufgabe 5: Lasse den Nutzer eine Zahl eingeben und berechne von dieser Zahl die Fakultät.
-
-
-            //Aufgabe 6a: Lasse den Nutzer einen Text eingeben und gebe den Text nur in Großbuchstaben aus
-            //Aufgabe 6b: Gebe die Länge des Textes aus
-            //Aufgabe 6c: Zähle wie oft der Buchstabe a vorkommt.
-            //Aufgabe 6d: Gebe nur einen Teil des Textes aus. Ab Position 3, und dann die nächsten 5 Buchstaben
+ 
 
 
 }
