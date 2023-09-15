ea90d250d5899e9b91bd0bdb37534354cf7e7605
Luca Schulz <luca.schulz@tecalliance.net>
2023-09-15 09:43:40 +0200

    Methoden2

--- Program.cs
+++ Program.cs
@@ -9,30 +9,65 @@ namespace HelloWorld
     class Program
     {
         static void Main(string[] args){
-        
-        int zahl1 = 10;
-        int zahl2 = 5;
+
         Console.WriteLine("Gebe mir ein Rechenzeichen");
+        string zahl1 = Console.ReadLine();
+        string zahl2 = Console.ReadLine();
+        
         string zeichen = Console.ReadLine();
         switch(zeichen)
         {
             case "+":
-            Addition(zahl1,zahl2);
-            break;
+                int ergebnis = AdditionwithReturn(zahl1,zahl2);
+                break;
             case "-":
-            Substraction(zahl1,zahl2);
-            break;
+                Substraction(zahl1,zahl2);
+                break;
             case "*":
-            Multiplication(zahl1,zahl2);
-            break;
+                Multiplication(zahl1,zahl2);
+                break;
             case "/":
-            Division(zahl1,zahl2);
-            break;
+                Division(zahl1,zahl2);
+                break;
             default:
                 Console.WriteLine("Ich kenne dieses Rechenzeichen nicht");
             break;
         }
                 }
+    
+    
+static int AdditionwithReturn(string zahl1, string zahl2)
+     {
+        int numero1 = Convert.ToInt16(zahl1);
+        int numero2 = Convert.ToInt16(zahl2);
+
+        return numero1+numero2;
+     }
+
+     static void Addition(string zahl1, string zahl2)
+     {
+        int numero1 = Convert.ToInt16(zahl1);
+        int numero2 = Convert.ToInt16(zahl2);
+    }
+
+    static void Substraction(string zahl1, string zahl2)
+     {
+        int numero1 = Convert.ToInt16(zahl1);
+        int numero2 = Convert.ToInt16(zahl2);
+    }
+
+    static void Multiplication(string zahl1, string zahl2)
+     {
+        int numero1 = Convert.ToInt16(zahl1);
+        int numero2 = Convert.ToInt16(zahl2);
+    }
+
+    static void Division(string zahl1, string zahl2)
+     {
+        int numero1 = Convert.ToInt16(zahl1);
+        int numero2 = Convert.ToInt16(zahl2);
+    }
+    
     
     static void Addition(int zahl1, int zahl2){
         Console.WriteLine(zahl1+zahl2);
