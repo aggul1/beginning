b2d03aa71b2cb3635c629ca2071976f50d9d1ac0
Luca Schulz <luca.schulz@tecalliance.net>
2023-09-15 11:51:12 +0200

    Rest  Aufgabe

--- Program.cs
+++ Program.cs
@@ -10,24 +10,10 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            //Frage den Nutzer wieviele Texte er in einem Array speichern will.
-            //Initalisiere das Array in der gewünschten größe
-            // string[] myText = new string[5];
-            //Lasse den Nutzer das Array nacheinander mit Texten befüllen.
-            //Gebe die Texte mit mindestens 20 Zeichen aus, fülle den Text mit Leerzeichen auf von Links wenn kleiner.
-            /* */
-            Console.WriteLine("Wie groß soll dein Array sein?");
-            int arrayanzahl = Convert.ToInt32(Console.ReadLine());
-            string[] myText = new string[arrayanzahl];
-            for(int position=0; position<arrayanzahl; position++)
-            {
-                string text1 = Console.ReadLine();
-                myText[position]=text1;
-            }
-            foreach(string entry in myText)
-            {
-                Console.WriteLine(entry.PadLeft(20,' '));
-            } 
+           Console.WriteLine("Gib mir 2 Zahlen");
+           int zahl1 = Convert.ToInt32(Console.ReadLine());
+           int zahl2 = Convert.ToInt32(Console.ReadLine());
+           Console.WriteLine(zahl1%zahl2);
         }
 }
 }       
