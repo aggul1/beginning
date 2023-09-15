0e68ca3a788598875f9b595b786d014bb0068c05
Luca Schulz <luca.schulz@tecalliance.net>
2023-09-15 13:08:29 +0200

    Array größte kleinste durchschnitt

--- Program.cs
+++ Program.cs
@@ -10,10 +10,28 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-           Console.WriteLine("Gib mir 2 Zahlen");
-           int zahl1 = Convert.ToInt32(Console.ReadLine());
-           int zahl2 = Convert.ToInt32(Console.ReadLine());
-           Console.WriteLine(zahl1%zahl2);
+
+        
+
+// Erstelle ein Array das Zahlen beinhaltet.
+            // Es sollen 5 Zahlen eingegeben werden können
+            // Befülle das Array in einer Schleife durch den Nutzer
+            // Gebe am Ende:
+            // - die kleinste Zahl aus
+            // - die größte Zahl
+            // - den Durchschnitt
+
+            int[] mynumber = new int[5];
+            for(int position=0; position < mynumber.Count(); position++)
+            {
+                int text1 = Convert.ToInt32(Console.ReadLine());
+                mynumber[position]=text1;
+            }
+          Console.WriteLine("--------------");  
+          Console.WriteLine(mynumber.Min());
+          Console.WriteLine(mynumber.Max());
+          Console.WriteLine(mynumber.Average());
+
         }
 }
 }       
