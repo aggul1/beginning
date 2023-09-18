87cd101c0c60e2e00bd7c29674f53cb91530b5d8
Luca Schulz <luca.schulz@tecalliance.net>
2023-09-18 15:54:31 +0200

    fibonacci

--- Program.cs
+++ Program.cs
@@ -6,20 +6,24 @@ using System.Security.Cryptography.X509Certificates;
 
 namespace HelloWorld
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
 
-          Console.WriteLine("Gib eine Zahl ein");
-          Console.WriteLine("------------------");
-          int zahl = Convert.ToInt32(Console.ReadLine());
-          Console.WriteLine("------------------");
-          for(; zahl>=0; zahl--)
-          {
-            Console.WriteLine(zahl);
-            
-          }
-}
-}
+      Console.WriteLine("Gib das Limit an, bis zu welchem die Fibonacci Reihe ausgegeben werden soll");
+      Console.WriteLine("------------------");
+      int limit = Convert.ToInt32(Console.ReadLine());
+      int zahl1 = 0;
+      int zahl2 = 1;
+      int ergebnis = 0;
+      for(; zahl1+zahl2 < limit;)
+      {
+        ergebnis = zahl1 + zahl2;
+        Console.WriteLine(ergebnis);
+        zahl1 = zahl2;
+        zahl2 = ergebnis;
+      }
+    }
+  }
 }
