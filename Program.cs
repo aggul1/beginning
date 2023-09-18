cc08bb7d5f9f8e288dc696d40de2a20bc0a101da
Luca Schulz <luca.schulz@tecalliance.net>
2023-09-18 16:13:03 +0200

    mississippi

--- Program.cs
+++ Program.cs
@@ -11,19 +11,14 @@ namespace HelloWorld
     static void Main(string[] args)
     {
 
-      Console.WriteLine("Gib das Limit an, bis zu welchem die Fibonacci Reihe ausgegeben werden soll");
+      Console.WriteLine("Zähle die Vorkommnisse der jeweiligen Buchstaben im Wort MissiSsiPpi");
       Console.WriteLine("------------------");
-      int limit = Convert.ToInt32(Console.ReadLine());
-      int zahl1 = 0;
-      int zahl2 = 1;
-      int ergebnis = 0;
-      for(; zahl1+zahl2 < limit;)
-      {
-        ergebnis = zahl1 + zahl2;
-        Console.WriteLine(ergebnis);
-        zahl1 = zahl2;
-        zahl2 = ergebnis;
+      string wort = "Mississippi";
+      int m = wort.Count(c => c == 'M');
+      int i = wort.Count(c => c == 'i');
+      int s = wort.Count(c => c == 's'|| c =='S');
+      int p = wort.Count(c => c == 'p'|| c =='P');
+      Console.WriteLine($"Im Wort MissiSsiPpi kommt der Buchstabe m {m} mal, i {i} mal, s {s} mal und p {p} mal vor.");
       }
     }
   }
-}
