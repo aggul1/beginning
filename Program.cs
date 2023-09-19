5f49d4013e86f4cb61de5cddc236162dacc0384b
Luca Schulz <luca.schulz@tecalliance.net>
2023-09-19 10:56:16 +0200

    Das Auto fährt

--- Program.cs
+++ Program.cs
@@ -5,26 +5,62 @@ using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 
+using System.Globalization;
+
 namespace HelloWorld
 {
-  class Program
-  {
-    static void Main(string[] args)
-    {
+    class Program
+    {        static void Main(string[] args)
+        {
+            /*
+            // Passe die Methode "Fahren" so an, dass 
+            //- bei einer Geschwindigkeit von 0 die Methode "steht" zurück gibt: Hinweis: return
+            //- bei einer Geschwindigkeit von 1-10 "schleicht" zurück gibt
+            //- bei einer Geschwindigkeit von 11-50 "fährt langsam" zurück gibt
+            //- bei einer Geschwindigkeit von 51-100 "fährt schnell" zurück gibt
+            //- bei einer Geschwindigkeit über 100 "rast" zurück gibt.
 
-      Console.WriteLine("Gib einen Zeitraum in Sekunden an:");
-      Console.WriteLine("------------------");
-      int sek = Convert.ToInt32(Console.ReadLine());
-      int days = sek / 86400;
-      int hours = sek % 86400 / 3600;
-      int min = sek % 86400 % 3600 / 60;
-      int sek1 = sek % 86400 % 3600 % 60 % 60;
+            //Bau die Ausgabe so, dass nach jedem Aufruf folgende Texte ausgegeben werden können. 
+            //Das Auto steht.
+            //Das Auto schleicht.
+            //Das Auto fährt langsam.
+            //Das Auto fährt schnell. 
+            //Das Auto rast.
+            */
+            Console.WriteLine("Wie schnell fährt das Auto?");
+            int geschwindigkeit = Convert.ToInt32(Console.ReadLine());
+            string kmh = Fahren(geschwindigkeit);
+            Console.WriteLine($"Das Auto {kmh} ");
+        }
 
-      Console.WriteLine($"Tage:{days}, Stunden: {hours}, Minuten: {min}, Sekunden: {sek1}");
-      }
+        static string Fahren(int geschwindigkeit)
+        {
+              if(geschwindigkeit==0)
+              {
+                return "steht.";
+              }
+              else if(geschwindigkeit<=10 && 1<=geschwindigkeit)
+              {
+                return "schleicht.";
+              }
+              else if(geschwindigkeit<=55 && 11<=geschwindigkeit)
+              {
+                return "fährt langsam.";
+              }
+              else if(geschwindigkeit<=100 && 51<=geschwindigkeit)
+              {
+                return "fährt schnell.";
+              }
+              else if(geschwindigkeit>100)
+              {
+                return "rast.";
+              }
+              else
+              {
+                return "Das Auto ist kaputt!!!";
+              }
+             
+      
+        }
     }
-  }
-
-///1 tag = 86400 sek
-///1 stunde = 3600 sek
-///1 1 min = 60 sek
+}
