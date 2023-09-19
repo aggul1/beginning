e5aa22fb365aebbb1be64cba3e7313b7d6f6981b
Luca Schulz <luca.schulz@tecalliance.net>
2023-09-19 08:01:59 +0200

    tage stunden minuten sekunden

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel;
 using System.Numerics;
+using System.Reflection.PortableExecutable;
 using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
@@ -11,14 +12,19 @@ namespace HelloWorld
     static void Main(string[] args)
     {
 
-      Console.WriteLine("Zähle die Vorkommnisse der jeweiligen Buchstaben im Wort MissiSsiPpi");
+      Console.WriteLine("Gib einen Zeitraum in Sekunden an:");
       Console.WriteLine("------------------");
-      string wort = "Mississippi";
-      int m = wort.Count(c => c == 'M');
-      int i = wort.Count(c => c == 'i');
-      int s = wort.Count(c => c == 's'|| c =='S');
-      int p = wort.Count(c => c == 'p'|| c =='P');
-      Console.WriteLine($"Im Wort MissiSsiPpi kommt der Buchstabe m {m} mal, i {i} mal, s {s} mal und p {p} mal vor.");
+      int sek = Convert.ToInt32(Console.ReadLine());
+      int days = sek / 86400;
+      int hours = sek % 86400 / 3600;
+      int min = sek % 86400 % 3600 / 60;
+      int sek1 = sek % 86400 % 3600 % 60 % 60;
+
+      Console.WriteLine($"Tage:{days}, Stunden: {hours}, Minuten: {min}, Sekunden: {sek1}");
       }
     }
   }
+
+///1 tag = 86400 sek
+///1 stunde = 3600 sek
+///1 1 min = 60 sek
